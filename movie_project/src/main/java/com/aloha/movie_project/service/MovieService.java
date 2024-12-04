2129213ca941effdc2ac0da5618f48d5cbb685e9
tnp22 <ldh3239@naver.com>
2024-12-04 10:30:57 +0900

    3차병합

--- movie_project/src/main/java/com/aloha/movie_project/service/MovieService.java
+++ movie_project/src/main/java/com/aloha/movie_project/service/MovieService.java
@@ -3,8 +3,23 @@ package com.aloha.movie_project.service;
 import java.util.List;
 
 import com.aloha.movie_project.domain.Movie;
+import com.github.pagehelper.PageInfo;
 
 public interface MovieService {
     public List<Movie> movieList();
     public List<Movie> expectList();
+
+    // 조회
+    public Movie select(String id) throws Exception;
+    // 생성
+    public int insert(Movie movie) throws Exception;
+    // 수정
+    public int update(Movie movie) throws Exception;
+    
+    //페이징 목록
+    public PageInfo<Movie> list(int page, int size) throws Exception;
+    // 검색까지
+    public PageInfo<Movie> list(int page, int size,String search) throws Exception;
+    // 권한 조회
+    public List<Movie> list() throws Exception;
 }
--- movie_project/src/main/java/com/aloha/movie_project/service/MovieServiceImpl.java
+++ movie_project/src/main/java/com/aloha/movie_project/service/MovieServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.aloha.movie_project.domain.Movie;
 import com.aloha.movie_project.mapper.MovieMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 @Service
 public class MovieServiceImpl implements MovieService {
@@ -25,5 +27,55 @@ public class MovieServiceImpl implements MovieService {
         List<Movie> movieList = movieMapper.expectList();
         return movieList;
     }
+
+    @Override
+    public int insert(Movie movie) throws Exception {
+        int result = movieMapper.insert(movie);
+        return result;
+    }
+
+    @Override
+    public List<Movie> list() throws Exception {
+        List<Movie> list = movieMapper.list();
+        return list;
+    }
+
+    @Override
+    public PageInfo<Movie> list(int page, int size) throws Exception {
+        // ⭐ PageHelper.startPage(현재 페이지, 페이지당 게시글 수);
+        PageHelper.startPage(page, size);
+        List<Movie> list = movieMapper.list();
+        
+        // ⭐ PageInfo<Board>( 리스트, 노출 페이지 개수 )
+        PageInfo<Movie> pageInfo = new PageInfo<Movie>(list, 5);
+        return pageInfo;
+    }
+
+    @Override
+    public PageInfo<Movie> list(int page, int size, String search) throws Exception {
+        // ⭐ PageHelper.startPage(현재 페이지, 페이지당 게시글 수);
+        PageHelper.startPage(page, size);
+        List<Movie> list = movieMapper.search(search);
+
+        // ⭐ PageInfo<Board>( 리스트, 노출 페이지 개수 )
+        PageInfo<Movie> pageInfo = new PageInfo<Movie>(list, 5);
+        return pageInfo;
+    }
+
+
+
+    @Override
+    public Movie select(String id) throws Exception {
+        Movie movie = movieMapper.select(id);
+        return movie;
+    }
+
+    @Override
+    public int update(Movie movie) throws Exception {
+        int result = movieMapper.update(movie);
+        return result;
+    }
+
+    
     
 }
