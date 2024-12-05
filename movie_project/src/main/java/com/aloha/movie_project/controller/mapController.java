2aaeaef39ee59881b973112b13124c718caa30e6
tnp22 <ldh3239@naver.com>
2024-12-05 14:37:00 +0900

    상영관 생성

--- movie_project/src/main/java/com/aloha/movie_project/controller/mapController.java
+++ movie_project/src/main/java/com/aloha/movie_project/controller/mapController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.aloha.movie_project.domain.FileText;
@@ -37,43 +39,11 @@ public class mapController {
         return "/admin/addMap";
     }
 
-    // 맵 저장하기
-    @ResponseBody
-    @PostMapping("/addMap")
-    public String addMap(@RequestBody addMap map) {
-        map.setMapSize(map.getX() * map.getY());
-
-        log.info("*******맵 : " + map);
-
-        // 맵 위치 확인 로직 예시
-        List<List<String>> mapData = map.getMapData();
-        // log.info("맵 위치 3.0 : " + mapData.get(3).get(0)); //출력결과 기본값 D_1 (4번째줄 첫번째값)
-
-        // 2차원 리스트를 문자열로 변환
-        StringBuilder sb = new StringBuilder();
-        for (List<String> row : mapData) {
-            if (sb.length() > 0) {
-                sb.append("\n"); // 행 구분자 추가
-            }
-            sb.append(String.join(",", row)); // 내부 리스트를 문자열로 변환
-        }
-        String result = sb.toString();
-
-        String path = "C:\\upload\\test"; // 파일 저장 경로
-        String fileName = "test.txt"; // 파일 이름
-
-        // text 파일로 저장
-        ft.write(path, fileName, result);
-
-        return "SUCCESS";
-    }
-
     // 불러오기
     @PutMapping("/addMap")
-    public ResponseEntity<Map<String, Object>> readMap() {
+    public ResponseEntity<Map<String, Object>> readMap(@RequestBody Map<String, String> requestData) {
         String path = "C:\\upload\\test"; // 파일 저장 경로
-        String fileName = "test.txt"; // 파일 이름
-
+        String fileName = requestData.get("fileName")+".txt"; // JSON에서 fileName 추출
         // 파일 읽기
         String result = ft.read(path, fileName);
         System.out.println(result);
