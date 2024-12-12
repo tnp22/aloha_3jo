94d88588c66339c124e8690bbf8090f27047106e
rlawnsrud310 <wnsrud310@gmail.com>
2024-12-12 18:00:11 +0900

    아하 커밋먼저 해야 메인가짐

--- movie_project/src/main/java/com/aloha/movie_project/controller/ticketController.java
+++ movie_project/src/main/java/com/aloha/movie_project/controller/ticketController.java
@@ -12,11 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.aloha.movie_project.domain.Cinema;
 import com.aloha.movie_project.domain.CustomUser;
@@ -299,6 +302,16 @@ public class ticketController {
         // model.addAttribute("movie", movie_);
         model.addAttribute("reservationList", reservationList);
         return "/user/reservationList";
+    }
+
+    @ResponseBody
+    @DeleteMapping("/delete")
+    public String deleteReserv(@RequestBody Map<String, String> data) {
+        String id = data.get("id");
+        int result = reserveService.delectReserve(id);
+        if (result > 0)
+            return "SUCCESS";
+        return "Fail";
     }
 
 }
